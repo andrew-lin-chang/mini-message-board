328708566c834d838ab26ab246e46cf58252287c
Chang <andrewchang@utexas.edu>
2023-07-11 17:13:18 -0500

    add message form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello world",
+    user: "Rizzler",
+    added: new Date()
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
+
+router.post("/", (req, res, next) => {
+  const message = {
+    text: req.body.messageText,
+    user: req.body.messageUser,
+    added: new Date()
+  }
+
+  messages.push(message)
+  console.log(message)
+  res.redirect('/')
+})
 
 module.exports = router;
